6ac13e239f305c8a155b556076e0c44feceb42cd
AiginaCoder <coder.aigina@gmail.com>
2015-01-02 12:53:46 +0900

    EAIntroViewをインストール

---
+++ Podfile
@@ -0,0 +1,2 @@
+platform :ios, "6.0"
+pod 'EAIntroView', '~> 2.6.0'
---
+++ Well-Known/DFTutorialViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DFTutorialViewController.swift
+//  Well-Known
+//
+//  Created by hw14d002 on 2015/01/02.
+//  Copyright (c) 2015年 DualFang. All rights reserved.
+//
+
+import UIKit
+
+class DFTutorialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
